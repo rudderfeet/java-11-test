c1294ce71fb91cbfc312a250f5685801eb7a4e93
Scott McCrory <scott@mccrory.us>
2018-12-29 21:19:37 -0500

    Initial import

---
+++ src/main/java/us/mccrory/java11test/Java11TestApplication.java
@@ -0,0 +1,14 @@
+package us.mccrory.java11test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11TestApplication.class, args);
+	}
+
+}
+
