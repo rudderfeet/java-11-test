76de963a6baaa599fcfb5eb413c8caa714464591
Scott McCrory <smccrory@Scotts-MacBook-Pro.local>
2020-05-12 16:32:05 -0400

    Works!  Trying name-based ingress routing

--- deployment/kubectl/kube-create-deployment.yaml.sh
+++
@@ -1,4 +0,0 @@
-# Have Kubernetes make an initial deployment.yaml for us
-#kubectl create deployment java-11-test --image=scottmccrory/java-11-test --dry-run -o=yaml > deployment.yaml
-#echo --- >> deployment.yaml
-#kubectl create service clusterip java-11-test --tcp=8081:8081 --dry-run -o=yaml >> deployment.yaml
